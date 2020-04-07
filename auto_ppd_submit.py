a73735901f63498a90849276c7e6592de6d7ea22
wpf999 <wpf999@yeah.net>
2020-04-07 22:11:48 +0800

    test

--- auto_ppd_submit.py
+++ auto_ppd_submit.py
@@ -6,6 +6,8 @@
 # release date: 20200405   #fix 5 bugs  
 # release date: 20200406   #detect ImportError
 # release date: 20200407   #fix bug in function get_gpu_list
+
+
 import sys
 if sys.version_info.major != 3 :
 	print( 'python3 is needed. \npress enter to exit...' )
