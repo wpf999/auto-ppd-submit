28c443d714ef034e2b70b88f97bea76880c973b4
wpf999 <wpf999@yeah.net>
2020-04-09 23:32:12 +0800

    test

--- import ssl.py
+++ import ssl.py
@@ -2,3 +2,13 @@ import ssl
 
 print(ssl.__name__)
 print(ssl.__file__)
+'''
+Version: 1.44.0 (system setup)
+Commit: 2aae1f26c72891c399f860409176fe435a154b13
+Date: 2020-04-07T23:30:48.287Z
+Electron: 7.1.11
+Chrome: 78.0.3904.130
+Node.js: 12.8.1
+V8: 7.8.279.23-electron.0
+OS: Windows_NT ia32 10.0.18363
+'''
